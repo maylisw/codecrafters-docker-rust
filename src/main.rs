6680f0f870f8ba251171893752f469500b262fa3
mw3391 <mw3391@columbia.edu>
2024-01-28 14:45:58 -0800

    pipe stdout + stderr to parent process

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
 use anyhow::{Context, Result};
+use std::{
+    io::Read,
+    process::{Command, Stdio},
+};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    // spawn new process
+    let process = Command::new(command)
         .args(command_args)
-        .output()
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
         .with_context(|| {
             format!(
-                "Tried to run '{}' with arguments {:?}",
+                "Failed to run '{}' with arguments {:?}",
                 command, command_args
             )
         })?;
-    
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
-    }
+
+    match process.stdout {
+        Some(mut output) => {
+            let mut std_out = String::new();
+            output
+                .read_to_string(&mut std_out)
+                .with_context(|| format!("Failed to read output '{:#?}' to string", output))?;
+            println!("{}", std_out);
+        }
+        None => todo!(),
+    };
+
+    match process.stderr {
+        Some(mut errput) => {
+            let mut std_err = String::new();
+            errput
+                .read_to_string(&mut std_err)
+                .with_context(|| format!("Failed to read errput '{:#?}' to string", errput))?;
+            eprintln!("{}", std_err);
+        }
+        None => todo!(),
+    };
 
     Ok(())
 }
